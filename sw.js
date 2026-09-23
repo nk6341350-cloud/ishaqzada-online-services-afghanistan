28b9894250a3987848ec7fffd156bd47dfd900d1
nk6341350-cloud <nk6341350@gmail.com>
2026-09-23 20:29:54 +0430

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='ishaqzada-all-before-db-20260918';self.addEventListener('install',e=>{self.skipWaiting()});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
